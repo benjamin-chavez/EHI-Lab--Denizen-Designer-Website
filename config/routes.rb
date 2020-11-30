296e525d47aebce9feaea74e82ab5a79ff005d37
Ben Chavez <ben.m.chavez@gmail.com>
2020-11-30 11:31:04 -0600

    tweak rails routes to send to React Router

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # resources :participants
-  get "/:id", to: 'pages#home' 
+  get "/denizendesigner/home", to: 'pages#home' 
+  get "/denizendesigner/interviews", to: 'pages#home' 
+  get "/denizendesigner/interviews/:id", to: 'pages#home' 
+  get "/denizendesigner/report", to: 'pages#home' 
+  get "/denizendesigner/designerdatabase", to: 'pages#home' 
+  get "/denizendesigner/quotes", to: 'pages#home' 
+  get "/denizendesigner/resources", to: 'pages#home' 
 
 
   # API
